47a2918416c67f6571c97c048564795154db9d10
wjdtngh950 <wjdtngh950@naver.com>
2022-01-21 22:48:49 +0900

    Create spring boot project

---
+++ src/main/java/com/blog/stockandcafebe/StockAndCafeBeApplication.java
@@ -0,0 +1,13 @@
+package com.blog.stockandcafebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAndCafeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAndCafeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/stockandcafebe/StockAndCafeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.stockandcafebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAndCafeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
