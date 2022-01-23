f54c2444cfce20f96b4b9ab68a99e4a2ddb9ec77
wjdtngh950 <wjdtngh950@naver.com>
2022-01-23 22:19:50 +0900

    Create Security Config

---
+++ src/main/java/com/blog/stockandcafebe/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.blog.stockandcafebe.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Slf4j
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/auth/all").permitAll();
+    }
+}
--- src/main/java/com/blog/stockandcafebe/controller/AuthController.java
+++ src/main/java/com/blog/stockandcafebe/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.blog.stockandcafebe.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @GetMapping("/all")
+    public void testAll() {
+        log.info("testAll..........");
+    }
+
+    @GetMapping("/member")
+    public void testMember() {
+        log.info("testMember..........");
+    }
+
+    @GetMapping("/admin")
+    public void testAdmin() {
+        log.info("testAdmin..........");
+    }
 }
