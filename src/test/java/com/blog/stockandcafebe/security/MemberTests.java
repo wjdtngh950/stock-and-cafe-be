612c7b51061d50f3ab00b9021f5fd559ae3d8267
wjdtngh950 <wjdtngh950@naver.com>
2022-01-24 16:04:00 +0900

    Test code for insert Member

---
+++ src/test/java/com/blog/stockandcafebe/security/MemberTests.java
@@ -0,0 +1,40 @@
+package com.blog.stockandcafebe.security;
+
+import com.blog.stockandcafebe.entity.Member;
+import com.blog.stockandcafebe.entity.MemberRole;
+import com.blog.stockandcafebe.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemberTests {
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void insertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Member member = Member.builder()
+                    .memberId("saddummy" + i)
+                    .email("saddummy" + i + "@gmail.com")
+                    .name("dummy" + i)
+                    .fromSocial(false)
+                    .password(passwordEncoder.encode("1234"))
+                    .build();
+            member.addMemberRole(MemberRole.USER);
+            if (i > 80)
+                member.addMemberRole(MemberRole.MANAGER);
+            if (i > 90)
+                member.addMemberRole(MemberRole.ADMIN);
+            repository.save(member);
+        });
+    }
+}
