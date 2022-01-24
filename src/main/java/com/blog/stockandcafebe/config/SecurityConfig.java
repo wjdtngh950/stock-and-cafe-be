732293f2854563968e2a8fad647332e5714ad1c2
wjdtngh950 <wjdtngh950@naver.com>
2022-01-24 15:40:36 +0900

    Disable CSRF Token

--- src/main/java/com/blog/stockandcafebe/config/SecurityConfig.java
+++ src/main/java/com/blog/stockandcafebe/config/SecurityConfig.java
@@ -19,5 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/auth/all").permitAll();
+        http.csrf().disable();
     }
 }
