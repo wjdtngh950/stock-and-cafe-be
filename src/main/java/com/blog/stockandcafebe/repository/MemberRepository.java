06bfd8d0e45b42d498292208524d5ae008d59ad6
wjdtngh950 <wjdtngh950@naver.com>
2022-01-24 16:05:24 +0900

    Create method for read Member

--- src/main/java/com/blog/stockandcafebe/repository/MemberRepository.java
+++ src/main/java/com/blog/stockandcafebe/repository/MemberRepository.java
@@ -1,7 +1,14 @@
 package com.blog.stockandcafebe.repository;
 
 import com.blog.stockandcafebe.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select m from Member m where m.fromSocial =:social and m.email =:email")
+    Optional<Member> findByEmail(String email, boolean social);
 }
