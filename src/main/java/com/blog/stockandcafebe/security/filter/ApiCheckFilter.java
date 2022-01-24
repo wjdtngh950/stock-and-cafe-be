caffce92e399cc2940dfae09b86f5f2c79e97604
wjdtngh950 <wjdtngh950@naver.com>
2022-01-24 20:45:11 +0900

    Add condition to ApiCheckFilter

--- src/main/java/com/blog/stockandcafebe/security/filter/ApiCheckFilter.java
+++ src/main/java/com/blog/stockandcafebe/security/filter/ApiCheckFilter.java
@@ -1,6 +1,9 @@
 package com.blog.stockandcafebe.security.filter;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,9 +11,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 public class ApiCheckFilter extends OncePerRequestFilter {
+
+    private AntPathMatcher antPathMatcher;
+    private String pattern;
+
+    public ApiCheckFilter(String pattern) {
+        this.antPathMatcher = new AntPathMatcher();
+        this.pattern = pattern;
+    }
 
     @Override
     protected void doFilterInternal(
@@ -18,7 +30,37 @@ public class ApiCheckFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        log.info("ApiCheckFilter..........");
+        if (antPathMatcher.match(pattern, request.getRequestURI())) {
+            log.info("ApiCheckFilter..........");
+
+            boolean checkHeader = checkAuthHeader(request);
+            if (checkHeader) {
+                filterChain.doFilter(request, response);
+                return;
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.setContentType("application/json;charset=utf-8");
+                PrintWriter out = response.getWriter();
+                JSONObject json = new JSONObject();
+                String message = "FAIL CHECK API TOKEN";
+                json.put("code", "403");
+                json.put("message", message);
+                out.print("{code: 403, message: fail");
+            }
+        }
         filterChain.doFilter(request, response);
+    }
+
+    private boolean checkAuthHeader(HttpServletRequest request) {
+        boolean checkResult = false;
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(authHeader)) {
+            log.info("Authorization exist: " + authHeader);
+            if (authHeader.equals("1234"))
+                checkResult = true;
+        }
+        return checkResult;
     }
 }
