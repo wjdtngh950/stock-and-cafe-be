c5c5d257055a1ef782dbc0f4c5cc53fd4d64cf80
wjdtngh950 <wjdtngh950@naver.com>
2022-01-24 15:57:15 +0900

    Apply role to Member entity

--- src/main/java/com/blog/stockandcafebe/entity/Member.java
+++ src/main/java/com/blog/stockandcafebe/entity/Member.java
@@ -2,9 +2,9 @@ package com.blog.stockandcafebe.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Builder
@@ -30,4 +30,11 @@ public class Member extends BaseEntity {
     @Column(name = "from_social", nullable = false)
     private boolean fromSocial;
 
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<MemberRole> roleSet = new HashSet<>();
+
+    public void addMemberRole(MemberRole memberRole) {
+        roleSet.add(memberRole);
+    }
 }
